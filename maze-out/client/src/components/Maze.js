b5fa642e5dfa936830493f146c9c3d5d870cf9bd
sahn1998 <sahn1998@gmail.com>
2020-12-02 01:26:01 -0800

    made everything more clean

--- maze-out/client/src/components/Maze.js
+++ maze-out/client/src/components/Maze.js
@@ -1,24 +1,71 @@
 import React from "react";
+import { Button } from '@material-ui/core';
 import { Tile } from "./Tile";
+import "./Maze.css";
 
 export class Maze extends React.Component {
 
     state = {
         // Creating a 2D Matrix for creating a maze grid.
-        maze: new Array(this.props.rows).fill('').map(x => (new Array(this.props.cols).fill(''))),
+        row: this.props.boardSize,
+        col: this.props.boardSize,
+        maze: new Array(this.props.boardSize).fill('').map(x => (new Array(this.props.boardSize).fill(''))),
+        startingPoint: false,
+        endPoint: false,
+        setWalls: false,
+    }
+
+    setStartingPointClick = () => {
+        if (!this.startingPoint) {
+            this.setState({
+                startingPoint: true,
+                endPoint: false,
+                setWalls: false,
+            });
+        } else {
+            this.setState({startingPoint: false});
+        }
+    }
+
+    setEndPointClick = () => {
+        if (!this.endPoint) {
+            this.setState({
+                endPoint: true,
+                startingPoint: false,
+                setWalls: false,
+            });
+        } else {
+            this.setState({endPoint: false});
+        }
+    }
+
+    setWallsClick = () => {
+        if (!this.startingPoint) {
+            this.setState({
+                setWalls: true,
+                startingPoint: false,
+                endPoint: false,
+            });
+        } else {
+            this.setState({setWalls: false});
+        }
     }
 
     createMaze = () => {
-        const mazeStyle = {
-            textAlign: "center",
-            height: "100%",
-            width: "100%",
-            tableLayout:"fixed",
-        };
-
-        const mazeGrid = this.state.maze;
         // Visualizing the maze grid with Tile component
         // For each row, all the columns are displayed with Tile component.
+
+        const style = {
+            margin:"auto",
+            height: "auto",
+            width:"auto",
+            border:".1px solid black",
+            tableLayout:'fixed',
+        }
+
+
+        const mazeGrid = this.state.maze;
+
         const maze = mazeGrid.map((row, i) => {
             return (
                 <tr>
@@ -33,7 +80,7 @@ export class Maze extends React.Component {
 
         return (
             <div>
-                <table cellSpacing="0" style={mazeStyle}>
+                <table cellSpacing="0" style={style}>
                     <tbody>
                         {maze}
                     </tbody>
@@ -42,13 +89,29 @@ export class Maze extends React.Component {
         )
     }
 
-    handleClick(){
-
-    }
-
-    render() {        
+    render() {
         return (
             <div>
+                <div className="buttons">
+                    <Button
+                        onClick={this.setStartingPointClick}
+                        className="starting-point-button"
+                    >
+                        Click and Set Starting Point
+                    </Button>
+                    <Button
+                        onClick={this.setEndPointClick}
+                        className="end-point-button"
+                    >
+                        Click and Set End Point
+                    </Button>
+                    <Button
+                        onClick={this.setWallsClick}
+                        className="set-walls-button"
+                    >
+                        Click and Set Walls
+                    </Button>
+                </div>
                 {this.createMaze()}
             </div>
         )
--- maze-out/client/src/components/Tile.js
+++ maze-out/client/src/components/Tile.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import "./Tile.css";
 
-export class Tile extends React.Component{
+export class Tile extends React.Component {
+
+    state = {
+        clicked: false,
+    }
+
     render() {
+        const tileStyle = {
+            overflow:'hidden',
+            width:'19px',
+            height:'19px',
+            backgroundColor:'black',
+        }
+
         return (
-            <td onClick={this.props.handleClick}>
-                <div
+            <td
+                style={tileStyle}
+            >
+                <button 
+                    onClick={this.props.handleClick}
                     style={{
-                        border: "1px solid",
-                        height:25,
+                        width:'100%',
+                        height:'100%',
                     }}
                 />
             </td>
